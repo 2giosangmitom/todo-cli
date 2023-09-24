1700d30d8da331ecc9689eb9e386f259b5ada3a1
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-09-24 14:35:04 +0700

    format

--- src/main.rs
+++ src/main.rs
@@ -6,162 +6,162 @@ pub use service::*;
 
 #[derive(Parser)]
 #[command(
-  name = "Todo CLI",
-  author = "Vo Quang Chien",
-  version = "1.0",
-  about = "Simple todo app written in Rust Programming Language!",
-  help_template = "{name} {version}\n{about}\nMaintainer: {author-with-newline}\n{usage-heading} {usage}\n{all-args}"
+    name = "Todo CLI",
+    author = "Vo Quang Chien",
+    version = "1.0",
+    about = "Simple todo app written in Rust Programming Language!",
+    help_template = "{name} {version}\n{about}\nMaintainer: {author-with-newline}\n{usage-heading} {usage}\n{all-args}"
 )]
 struct CliArgs {
-  /// Name of todo item want to add
-  #[arg(short, long, value_name = "item_name")]
-  add: Option<String>,
+    /// Name of todo item want to add
+    #[arg(short, long, value_name = "item_name")]
+    add: Option<String>,
 
-  /// Id of item want to mark as completed
-  #[arg(short, long, value_name = "item_id")]
-  complete: Option<u32>,
+    /// Id of item want to mark as completed
+    #[arg(short, long, value_name = "item_id")]
+    complete: Option<u32>,
 
-  /// Id of item want to mark as uncompleted
-  #[arg(short, long, value_name = "item_id")]
-  uncomplete: Option<u32>,
+    /// Id of item want to mark as uncompleted
+    #[arg(short, long, value_name = "item_id")]
+    uncomplete: Option<u32>,
 
-  /// Id of item want to delete,
-  /// note that it won't destroy data record,
-  /// if you want to destory an item, use '--destroy' option.
-  #[arg(short, long, value_name = "item_id")]
-  delete: Option<u32>,
+    /// Id of item want to delete,
+    /// note that it won't destroy data record,
+    /// if you want to destory an item, use '--destroy' option.
+    #[arg(short, long, value_name = "item_id")]
+    delete: Option<u32>,
 
-  /// Restore a deleted todo item
-  #[arg(short, long, value_name = "item_id")]
-  restore: Option<u32>,
+    /// Restore a deleted todo item
+    #[arg(short, long, value_name = "item_id")]
+    restore: Option<u32>,
 
-  /// Id of item to want destroy, this will actually destroy the data record,
-  #[arg(long, value_name = "item_id")]
-  destroy: Option<u32>,
+    /// Id of item to want destroy, this will actually destroy the data record,
+    #[arg(long, value_name = "item_id")]
+    destroy: Option<u32>,
 
-  /// Destory all 'deleted' marked todo items,
-  /// this will actually destroy the data record,
-  #[arg(long)]
-  destroy_deleted: bool,
+    /// Destory all 'deleted' marked todo items,
+    /// this will actually destroy the data record,
+    #[arg(long)]
+    destroy_deleted: bool,
 
-  /// Clear all records, make all list empty
-  #[arg(long, value_name = "item_id")]
-  clear: bool,
+    /// Clear all records, make all list empty
+    #[arg(long, value_name = "item_id")]
+    clear: bool,
 
-  /// List todo items
-  #[arg(short, long, value_name = "list_type")]
-  list: Option<Option<ListType>>,
+    /// List todo items
+    #[arg(short, long, value_name = "list_type")]
+    list: Option<Option<ListType>>,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 enum ListType {
-  /// All todo items
-  All,
+    /// All todo items
+    All,
 
-  /// All completed todo tiems [default]
-  Completed,
+    /// All completed todo tiems [default]
+    Completed,
 
-  /// All uncompleted todo tiems
-  Uncompleted,
+    /// All uncompleted todo tiems
+    Uncompleted,
 
-  /// All deleted todo tiems
-  Deleted,
+    /// All deleted todo tiems
+    Deleted,
 }
 
 fn main() {
-  let args = CliArgs::parse();
+    let args = CliArgs::parse();
 
-  if let Some(name) = args.add {
-    match add_item(&name) {
-      Ok(s) => println!("{s}"),
-      Err(e) => eprintln!("Add '{name}' fail: {e}"),
+    if let Some(name) = args.add {
+        match add_item(&name) {
+            Ok(s) => println!("{s}"),
+            Err(e) => eprintln!("Add '{name}' fail: {e}"),
+        }
     }
-  }
 
-  if let Some(id) = args.complete {
-    match complete_item(id) {
-      Ok(s) => println!("{s}"),
-      Err(e) => eprintln!("Complete todo '{id}' fail: {e}"),
+    if let Some(id) = args.complete {
+        match complete_item(id) {
+            Ok(s) => println!("{s}"),
+            Err(e) => eprintln!("Complete todo '{id}' fail: {e}"),
+        }
     }
-  }
 
-  if let Some(id) = args.uncomplete {
-    match uncomplete_item(id) {
-      Ok(s) => println!("{s}"),
-      Err(e) => eprintln!("Uncomplete todo '{id}' fail: {e}"),
+    if let Some(id) = args.uncomplete {
+        match uncomplete_item(id) {
+            Ok(s) => println!("{s}"),
+            Err(e) => eprintln!("Uncomplete todo '{id}' fail: {e}"),
+        }
     }
-  }
 
-  if let Some(id) = args.delete {
-    match delete_item(id) {
-      Ok(s) => println!("{s}"),
-      Err(e) => eprintln!("Delete todo '{id}' fail: {e}"),
+    if let Some(id) = args.delete {
+        match delete_item(id) {
+            Ok(s) => println!("{s}"),
+            Err(e) => eprintln!("Delete todo '{id}' fail: {e}"),
+        }
     }
-  }
 
-  if let Some(id) = args.restore {
-    match restore_item(id) {
-      Ok(s) => println!("{s}"),
-      Err(e) => eprintln!("Restore todo '{id}' fail: {e}"),
+    if let Some(id) = args.restore {
+        match restore_item(id) {
+            Ok(s) => println!("{s}"),
+            Err(e) => eprintln!("Restore todo '{id}' fail: {e}"),
+        }
     }
-  }
 
-  if let Some(id) = args.destroy {
-    match destroy_item(id) {
-      Ok(s) => println!("{s}"),
-      Err(e) => eprintln!("Destroy todo '{id}' fail: {e}"),
-    };
-  }
-
-  if args.destroy_deleted {
-    match destroy_deleted() {
-      Ok(s) => println!("{s}"),
-      Err(e) => eprintln!("Destroy all deleted todos fail: {e}"),
-    };
-  }
-
-  if args.clear {
-    match clear() {
-      Ok(s) => println!("{s}"),
-      Err(e) => eprintln!("Clear all todos fail: {e}"),
-    };
-  }
-
-  let mut already_listed = false;
-
-  if let Some(None) = args.list {
-    default_list();
-    already_listed = true;
-  }
-
-  if let Some(Some(list_type)) = args.list {
-    use ListType::*;
-    match list_type {
-      All => match list_all() {
-        Ok(s) => println!("{s}"),
-        Err(e) => eprint!("List all todos fail: {e}"),
-      },
-      Completed => match list_completed() {
-        Ok(s) => println!("{s}"),
-        Err(e) => eprint!("List completed todos fail: {e}"),
-      },
-      Uncompleted => default_list(),
-      Deleted => match list_deleted() {
-        Ok(s) => println!("{s}"),
-        Err(e) => eprint!("List deleted todos fail: {e}"),
-      },
+    if let Some(id) = args.destroy {
+        match destroy_item(id) {
+            Ok(s) => println!("{s}"),
+            Err(e) => eprintln!("Destroy todo '{id}' fail: {e}"),
+        };
     }
-    already_listed = true;
-  }
 
-  if !already_listed {
-    default_list();
-  }
+    if args.destroy_deleted {
+        match destroy_deleted() {
+            Ok(s) => println!("{s}"),
+            Err(e) => eprintln!("Destroy all deleted todos fail: {e}"),
+        };
+    }
+
+    if args.clear {
+        match clear() {
+            Ok(s) => println!("{s}"),
+            Err(e) => eprintln!("Clear all todos fail: {e}"),
+        };
+    }
+
+    let mut already_listed = false;
+
+    if let Some(None) = args.list {
+        default_list();
+        already_listed = true;
+    }
+
+    if let Some(Some(list_type)) = args.list {
+        use ListType::*;
+        match list_type {
+            All => match list_all() {
+                Ok(s) => println!("{s}"),
+                Err(e) => eprint!("List all todos fail: {e}"),
+            },
+            Completed => match list_completed() {
+                Ok(s) => println!("{s}"),
+                Err(e) => eprint!("List completed todos fail: {e}"),
+            },
+            Uncompleted => default_list(),
+            Deleted => match list_deleted() {
+                Ok(s) => println!("{s}"),
+                Err(e) => eprint!("List deleted todos fail: {e}"),
+            },
+        }
+        already_listed = true;
+    }
+
+    if !already_listed {
+        default_list();
+    }
 }
 
 fn default_list() {
-  match list_uncompleted() {
-    Ok(s) => println!("{s}"),
-    Err(e) => eprint!("List uncompleted todos fail: {e}"),
-  }
+    match list_uncompleted() {
+        Ok(s) => println!("{s}"),
+        Err(e) => eprint!("List uncompleted todos fail: {e}"),
+    }
 }
--- src/storage.rs
+++ src/storage.rs
@@ -11,235 +11,233 @@ const CSV_FILE_NAME: &str = "todo.csv";
 type Result<T> = std::result::Result<T, StorageError>;
 
 pub fn add_item(item: Item) -> Result<String> {
-  let mut csv = Csv::new()?;
-  csv.file.seek(std::io::SeekFrom::End(0))?;
-  writeln!(csv.file, "{}", item.to_string())?;
-  Ok("Ok".to_string())
+    let mut csv = Csv::new()?;
+    csv.file.seek(std::io::SeekFrom::End(0))?;
+    writeln!(csv.file, "{}", item.to_string())?;
+    Ok("Ok".to_string())
 }
 
 pub fn update_item(item: Item) -> Result<()> {
-  let to_update_item = get_item_by_id(item.id())?;
-  let offset = get_offset_by_id(item.id())?;
-  Csv::new()?.splice(
-    offset as u64,
-    to_update_item.to_string().len() as u64,
-    &item.to_string(),
-  )
+    let to_update_item = get_item_by_id(item.id())?;
+    let offset = get_offset_by_id(item.id())?;
+    Csv::new()?.splice(
+        offset as u64,
+        to_update_item.to_string().len() as u64,
+        &item.to_string(),
+    )
 }
 
 pub fn delete_item(id: u32) -> Result<()> {
-  let to_delete_item = get_item_by_id(id)?;
-  let offset = get_offset_by_id(id)?;
-  Csv::new()?.splice(
-    offset as u64,
-    to_delete_item.to_string().len() as u64 + 1,
-    "",
-  )
+    let to_delete_item = get_item_by_id(id)?;
+    let offset = get_offset_by_id(id)?;
+    Csv::new()?.splice(
+        offset as u64,
+        to_delete_item.to_string().len() as u64 + 1,
+        "",
+    )
 }
 
 pub fn get_all() -> Result<Vec<Item>> {
-  Ok(
-    Csv::new()?
-      .content()?
-      .lines()
-      .filter_map(|line| line.parse::<Item>().ok())
-      .collect(),
-  )
+    Ok(Csv::new()?
+        .content()?
+        .lines()
+        .filter_map(|line| line.parse::<Item>().ok())
+        .collect())
 }
 
 pub fn get_item_by_id(id: u32) -> Result<Item> {
-  let content = Csv::new()?.content()?;
-  let item_str = content.lines().find(|line| {
-    if let Ok(item) = line.parse::<Item>() {
-      item.id() == id
-    } else {
-      false
-    }
-  });
+    let content = Csv::new()?.content()?;
+    let item_str = content.lines().find(|line| {
+        if let Ok(item) = line.parse::<Item>() {
+            item.id() == id
+        } else {
+            false
+        }
+    });
 
-  if let Some(item_str) = item_str {
-    Ok(item_str.parse().unwrap())
-  } else {
-    Err(StorageError::ItemNoExist(id))
-  }
+    if let Some(item_str) = item_str {
+        Ok(item_str.parse().unwrap())
+    } else {
+        Err(StorageError::ItemNoExist(id))
+    }
 }
 
 pub fn get_max_id() -> Result<u32> {
-  let max_id = get_all()?.iter().map(|item| item.id()).reduce(u32::max);
+    let max_id = get_all()?.iter().map(|item| item.id()).reduce(u32::max);
 
-  if let Some(max_id) = max_id {
-    Ok(max_id)
-  } else {
-    Ok(0)
-  }
+    if let Some(max_id) = max_id {
+        Ok(max_id)
+    } else {
+        Ok(0)
+    }
 }
 
 #[allow(unused)]
 struct Csv {
-  filename: String,
-  file: File,
+    filename: String,
+    file: File,
 }
 
 impl Csv {
-  fn new() -> Result<Self> {
-    let filename = Csv::filename()?;
-    let path = Path::new(&filename);
+    fn new() -> Result<Self> {
+        let filename = Csv::filename()?;
+        let path = Path::new(&filename);
 
-    if !path.exists() {
-      let mut file = Csv::create(path)?;
-      file.write_all(b"id,name,completed,deleted,createdAt,completedAt,deletedAt\n")?;
-      Ok(Self {
-        filename: filename.to_string(),
-        file,
-      })
-    } else {
-      Ok(Self {
-        filename: filename.to_string(),
-        file: Csv::open(path)?,
-      })
+        if !path.exists() {
+            let mut file = Csv::create(path)?;
+            file.write_all(b"id,name,completed,deleted,createdAt,completedAt,deletedAt\n")?;
+            Ok(Self {
+                filename: filename.to_string(),
+                file,
+            })
+        } else {
+            Ok(Self {
+                filename: filename.to_string(),
+                file: Csv::open(path)?,
+            })
+        }
     }
-  }
 
-  fn create(path: &Path) -> Result<fs::File> {
-    let csv = OpenOptions::new()
-      .read(true)
-      .write(true)
-      .create(true)
-      .open(path)?;
-    Ok(csv)
-  }
+    fn create(path: &Path) -> Result<fs::File> {
+        let csv = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(path)?;
+        Ok(csv)
+    }
 
-  fn open(path: &Path) -> Result<fs::File> {
-    let csv = OpenOptions::new().read(true).write(true).open(path)?;
-    Ok(csv)
-  }
+    fn open(path: &Path) -> Result<fs::File> {
+        let csv = OpenOptions::new().read(true).write(true).open(path)?;
+        Ok(csv)
+    }
 
-  fn filename() -> Result<String> {
-    let home = env::var("HOME")?;
-    let filename = home + "/" + CSV_FILE_NAME;
-    Ok(filename)
-  }
+    fn filename() -> Result<String> {
+        let home = env::var("HOME")?;
+        let filename = home + "/" + CSV_FILE_NAME;
+        Ok(filename)
+    }
 
-  fn content(&mut self) -> Result<String> {
-    let mut contents = String::new();
-    self.file.read_to_string(&mut contents)?;
-    Ok(contents)
-  }
+    fn content(&mut self) -> Result<String> {
+        let mut contents = String::new();
+        self.file.read_to_string(&mut contents)?;
+        Ok(contents)
+    }
 
-  fn splice(&mut self, offset: u64, delete_size: u64, write_content: &str) -> Result<()> {
-    use std::io::SeekFrom;
-    let file = &self.file;
-    let mut reader = BufReader::new(file);
-    reader.seek(SeekFrom::Start(offset + delete_size))?;
-    let mut rest_content = String::new();
-    reader.read_to_string(&mut rest_content)?;
-    let write_content = write_content.to_owned() + &rest_content;
-    let mut writer = BufWriter::new(file);
-    writer.seek(SeekFrom::Start(offset))?;
-    writer.write_all(write_content.as_bytes())?;
-    file.set_len(offset + write_content.len() as u64)?;
+    fn splice(&mut self, offset: u64, delete_size: u64, write_content: &str) -> Result<()> {
+        use std::io::SeekFrom;
+        let file = &self.file;
+        let mut reader = BufReader::new(file);
+        reader.seek(SeekFrom::Start(offset + delete_size))?;
+        let mut rest_content = String::new();
+        reader.read_to_string(&mut rest_content)?;
+        let write_content = write_content.to_owned() + &rest_content;
+        let mut writer = BufWriter::new(file);
+        writer.seek(SeekFrom::Start(offset))?;
+        writer.write_all(write_content.as_bytes())?;
+        file.set_len(offset + write_content.len() as u64)?;
 
-    Ok(())
-  }
+        Ok(())
+    }
 }
 
 #[allow(unused)]
 mod tests {
-  use super::*;
-  use std::io::Read;
+    use super::*;
+    use std::io::Read;
 
-  #[test]
-  fn create_file() -> Result<()> {
-    let csv = Csv::new()?;
-    Ok(())
-  }
+    #[test]
+    fn create_file() -> Result<()> {
+        let csv = Csv::new()?;
+        Ok(())
+    }
 
-  #[test]
-  fn title_line() -> Result<()> {
-    let mut csv = Csv::new()?;
-    let content = csv.content()?;
-    let lines: Vec<&str> = content.lines().collect();
-    assert!(!lines.is_empty());
-    assert_eq!(
-      lines[0],
-      "id,name,completed,deleted,createdAt,completedAt,deletedAt"
-    );
+    #[test]
+    fn title_line() -> Result<()> {
+        let mut csv = Csv::new()?;
+        let content = csv.content()?;
+        let lines: Vec<&str> = content.lines().collect();
+        assert!(!lines.is_empty());
+        assert_eq!(
+            lines[0],
+            "id,name,completed,deleted,createdAt,completedAt,deletedAt"
+        );
 
-    Ok(())
-  }
+        Ok(())
+    }
 }
 
 #[derive(Debug)]
 pub enum StorageError {
-  FileHandle(FileHandleError),
-  ParseItem(ParseItemError),
-  ItemNoExist(u32),
+    FileHandle(FileHandleError),
+    ParseItem(ParseItemError),
+    ItemNoExist(u32),
 }
 
 #[derive(Debug)]
 pub enum FileHandleError {
-  EnvVar(VarError),
-  Io(std::io::Error),
+    EnvVar(VarError),
+    Io(std::io::Error),
 }
 
 impl Display for StorageError {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    use FileHandleError::*;
-    use StorageError::*;
-    match &self {
-      FileHandle(source) => match source {
-        EnvVar(e) => write!(f, "Todo storage file handle env var error: {}", e),
-        Io(e) => write!(f, "Todo storage file handle io error: {}", e),
-      },
-      ParseItem(e) => write!(f, "Todo storage parse todo error: {}", e),
-      ItemNoExist(id) => write!(f, "Todo storage todo no exist: {}", id),
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use FileHandleError::*;
+        use StorageError::*;
+        match &self {
+            FileHandle(source) => match source {
+                EnvVar(e) => write!(f, "Todo storage file handle env var error: {}", e),
+                Io(e) => write!(f, "Todo storage file handle io error: {}", e),
+            },
+            ParseItem(e) => write!(f, "Todo storage parse todo error: {}", e),
+            ItemNoExist(id) => write!(f, "Todo storage todo no exist: {}", id),
+        }
     }
-  }
 }
 
 impl Error for StorageError {
-  fn source(&self) -> Option<&(dyn Error + 'static)> {
-    use FileHandleError::*;
-    use StorageError::*;
-    match self {
-      FileHandle(source) => match source {
-        EnvVar(source) => Some(source),
-        Io(source) => Some(source),
-      },
-      ParseItem(e) => Some(e),
-      ItemNoExist(_) => None,
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        use FileHandleError::*;
+        use StorageError::*;
+        match self {
+            FileHandle(source) => match source {
+                EnvVar(source) => Some(source),
+                Io(source) => Some(source),
+            },
+            ParseItem(e) => Some(e),
+            ItemNoExist(_) => None,
+        }
     }
-  }
 }
 
 impl From<VarError> for StorageError {
-  fn from(value: VarError) -> Self {
-    Self::FileHandle(FileHandleError::EnvVar(value))
-  }
+    fn from(value: VarError) -> Self {
+        Self::FileHandle(FileHandleError::EnvVar(value))
+    }
 }
 
 impl From<std::io::Error> for StorageError {
-  fn from(value: std::io::Error) -> Self {
-    Self::FileHandle(FileHandleError::Io(value))
-  }
+    fn from(value: std::io::Error) -> Self {
+        Self::FileHandle(FileHandleError::Io(value))
+    }
 }
 
 impl From<model::ParseItemError> for StorageError {
-  fn from(value: model::ParseItemError) -> Self {
-    Self::ParseItem(value)
-  }
+    fn from(value: model::ParseItemError) -> Self {
+        Self::ParseItem(value)
+    }
 }
 
 fn get_offset_by_id(id: u32) -> Result<usize> {
-  let mut csv = Csv::new()?;
-  let content = csv.content()?;
-  let prev_lines = content.lines().take_while(|line| {
-    if let Ok(item) = line.parse::<Item>() {
-      item.id() != id
-    } else {
-      true
-    }
-  });
-  let offset: usize = prev_lines.map(|line| line.len() + 1).sum();
-  Ok(offset)
+    let mut csv = Csv::new()?;
+    let content = csv.content()?;
+    let prev_lines = content.lines().take_while(|line| {
+        if let Ok(item) = line.parse::<Item>() {
+            item.id() != id
+        } else {
+            true
+        }
+    });
+    let offset: usize = prev_lines.map(|line| line.len() + 1).sum();
+    Ok(offset)
 }
